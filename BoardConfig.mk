9c2214b616b85b3ca820cc2c4359c7a1d6e86381
Captain Throwback <captainthrowback@hotmail.com>
2019-09-05 18:10:42 -0400

    nx609j: enable debugging flags
    - enabling toybox did not fix decryption
    - adding logcat for additional debugging
    
    Change-Id: I85b80ab9baa1717219fe3700b899bf9a48e468c8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -101,10 +101,10 @@ TW_NEW_ION_HEAP := true
 TW_NO_USB_STORAGE := true
 TARGET_RECOVERY_DEVICE_MODULES := tzdata android.hidl.base@1.0
 TW_RECOVERY_ADDITIONAL_RELINK_FILES := $(OUT)/system/usr/share/zoneinfo/tzdata $(OUT)/system/lib64/android.hidl.base@1.0.so
-TW_USE_TOOLBOX := true
 
 # TWRP Debug Flags
-#TARGET_USES_LOGD := true
-#TWRP_INCLUDE_LOGCAT := true
-#TARGET_RECOVERY_DEVICE_MODULES += debuggerd
-#TW_RECOVERY_ADDITIONAL_RELINK_FILES += $(OUT)/system/bin/debuggerd
+TARGET_USES_LOGD := true
+TWRP_INCLUDE_LOGCAT := true
+TARGET_RECOVERY_DEVICE_MODULES += debuggerd
+TW_RECOVERY_ADDITIONAL_RELINK_FILES += $(OUT)/system/bin/debuggerd
+
